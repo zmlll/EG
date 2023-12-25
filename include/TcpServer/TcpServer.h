bcd9af3a09a7ad83deaab86336b10778c93e80a1
zmlll <798479987@qq.com>
2023-12-25 22:26:27 +0800

    Eighth

---
+++ include/TcpServer/TcpServer.h
@@ -0,0 +1,39 @@
+#ifndef TCP_SERVER_H
+#define TCP_SERVER_H
+#include "Acceptor.h"
+#include "event_loop.h"
+
+class TcpServer
+{
+public:
+    TcpServer(const string &ip,)
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ include/ThreadPool/NoCopyable.h
@@ -0,0 +1,15 @@
+#ifndef NOCOPYABLE_H
+#define NOCOPYABLE_H
+
+class NoCopyable
+{
+protected:
+    NoCopyable(){}
+    ~NoCopyable(){}
+    NoCopyable(const NoCopyable &rhs) = delete;
+    NoCopyable &operator = (const NoCopyable &rhs);
+
+};
+
+
+#endif
