190b38a654c27cb6ad8bb9f202ecba2927567a84
zmlll <zmlii23@163.com>
2023-12-25 21:43:04 +0800

    fifth

--- include/TcpServer/Acceptor.h
+++ include/TcpServer/Acceptor.h
@@ -1,25 +1,33 @@
 #ifndef ACCEPTOR_H
 #define ACCEPTOR_H
+#include "Socket.h"
+#include "InetAddress.h"
 
+class Acceptor
+{
 
+private:
+    Socket _sock;
+    InetAdress _servAddr;
 
+public:
+    Acceptor(const string &,unsigned short);
+    void ready();
+    int accept();
+    int fd()
+    {
+        return _sock.fd();
 
+    }
 
+private:
+    //设置复用地址
+    void setReuseAddr();
+    //设置复用端口
+    void setReusePort();
+    void bind();
+    void listen();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 #endif
