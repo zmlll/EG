9557c9da9346dc9ef4e3ff7c238ea9118c4cffee
zmlll <798479987@qq.com>
2023-12-26 12:51:37 +0800

    13

---
+++ include/ThreadPool/NoCopyable.h
@@ -0,0 +1,16 @@
+#ifndef NOCOPYABLE_H
+#define NOCOPYABLE_H
+
+
+class NoCopyable
+{
+protected:
+    NoCopyable() {}
+    ~NoCopyable() {}
+    NoCopyable(const NoCopyable &rhs) = delete;
+    NoCopyable &operator= (const NoCopyable &rhs) = delete;
+
+};
+
+
+#endif
