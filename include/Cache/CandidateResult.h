9c351237dba3f33e54bcc08a3474db36f5dcbfeb
zmlll <798479987@qq.com>
2024-03-06 09:44:16 +0800

    candidateresult.h

---
+++ include/Cache/CandidateResult.h
@@ -0,0 +1,17 @@
+#ifndef CANDIDATERESULT_H
+#define CANDIDATERESULT_H
+
+#include <string.h>
+using namespace std;
+#include <iostream>
+struct CandidateResult
+{
+
+    string _word;
+    int _freq;
+    int _dist;
+
+};
+
+
+#endif
