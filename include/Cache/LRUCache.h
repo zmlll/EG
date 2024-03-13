ecbb2a3bbe2065c165d9100135b7a00e32d3d025
zmlll <798479987@qq.com>
2024-03-13 22:30:09 +0800

    LRUCache.h

---
+++ include/Cache/LRUCache.h
@@ -0,0 +1,39 @@
+#ifndef LRUCACHE_H
+#define LRUCACHE_H
+#include "CandidateResult.h"
+#include "../time.h"
+#include "../ProtocolParser.h"
+#include <list>
+using nlohmann::json;
+
+class LRUCache
+{
+public:
+    friend class CacheManager;
+    friend class KeyRecommander;
+    LRUCache(int capacity =3):_capacity(capacity),_isUpdating(false){}
+    LRUCache(const LRUCache& cache);
+    bool get(string key,json& value);
+    void put(string key,json& value);
+    void writeToFile(string filename);
+    void writeToFile(string filename,string key,json value);
+    void readFromFile(string filename);
+    void update(const LRUCache&);
+    void show();
+    list<pair<string,json>>& getPendingUpdateList();
+    list<pair<string,json>>& getResultList();
+
+
+private:
+    //采用hashtable查找
+    unordered_map<string,list<pair<string,json>>::iterator> _umap;
+    //保存键值对
+    list<pair<string,json>> _resultList;
+    //等待更新的节点信息
+    list<pair<string,json>> _pendingUpdateList;
+    int _capacity;
+    bool _isUpdating;
+
+};
+
+#endif
